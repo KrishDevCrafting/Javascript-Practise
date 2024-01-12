1d2adda4c0bb4ab714f2f1c8420ddddcfb337ab2
Krish <Krishkumer150@gamil.com>
2024-01-13 00:16:32 +0530

    ..

--- lessonFunction.js
+++ lessonFunction.js
@@ -1,6 +1,16 @@
+// Function in javascript
+
 function value(a, b) {
   let sum = a + b;
   console.log(sum);
 }
 
 value(5, 5);
+
+function listArrayItems(arr) {
+  for (var i = 0; i < arr.length; i++) {
+    console.log(i + 1, arr[i]);
+  }
+}
+var colors = ["red", "orange", "yellow", "green", "blue", "purple", "pink"];
+listArrayItems(colors);
