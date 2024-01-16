1a4ae86f98e921682682c38b01a2adc0fdb3b4c6
Krish <Krishkumer150@gamil.com>
2024-01-16 15:26:26 +0530

    Object-add

--- Object.js
+++ Object.js
@@ -1,9 +1,41 @@
-let keyvalue = {
-  Model: "BMW - M4",
-  Engin: {
-    V8: "4cyender",
-  },
-  Power: "6700Bhp",
+// let keyvalue = {
+//   Model: "BMW - M4",
+//   Engin: {
+//     V8: "4cyender",
+//   },
+//   Power: "6700Bhp",
+// };
+
+// console.log(keyvalue.Model, keyvalue.Engin);
+
+//Topic Object literals and the brackets notation 16/1/24
+var house2 = {};
+
+house2["room"] = 2;
+house2["color"] = "black";
+house2["color"] = "red";
+house2["vaishu"] = "Young-girl";
+console.log(house2);
+
+var arrOfKeys = ["speed", "altitude", "color", "height"];
+// var drone = {
+//   speed: 100,
+//   altitude: 200,
+//   color: "red",
+//   height: "700m",
+// };
+// for (var i = 0; i < arrOfKeys.length; i++) {
+//   console.log(drone[arrOfKeys[i]]);
+// }
+
+let example = ["tire", "Engine", "Headlight", "color"];
+var Car = {
+  tire: 4,
+  Engine: "4Piston cylinder",
+  color: "black",
+  Headlight: "four-Lights",
 };
 
-console.log(keyvalue.Model, keyvalue.Engin);
+for (let c = 0; c < example.length; c++) {
+  console.log(Car[example[c]]);
+}
