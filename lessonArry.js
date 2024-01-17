7237775e43601fb5045d5c5a80e2d4ab23390f96
Krish <Krishkumer150@gamil.com>
2024-01-17 13:10:02 +0530

    arry

--- lessonArry.js
+++ lessonArry.js
@@ -1,3 +1,7 @@
+//Using Array Literal
+// let power = ["one", "two", "three", "four"];
+// console.log([...power]);
+
 // const gojo = () => {
 //   let arr = ["one", null, 77];
 //   let arr2 = ["two", "three", "four", 5];
@@ -17,14 +21,14 @@
 // const result = sum(...numbers);
 
 // console.log(result); // Output: 6
-const value = (x, y, z) => {
-  return x + y + z;
-};
-// 1. single value using array
-const num = [10, 20, 40];
+// const value = (x, y, z) => {
+//   return x + y + z;
+// };
+// // 1. single value using array
+// const num = [10, 20, 40];
 
-const result = value(...num);
-console.log(result);
+// const result = value(...num);
+// console.log(result);
 
 // 2. Combining Arrays:
 // const _array = ["Gojo", "Naruto", "Denji"];
@@ -32,3 +36,31 @@ console.log(result);
 
 // const combined = [..._array, ..._array2];
 // console.log(combined);
+
+// Topic Arrays are Objects 17/1/24
+
+//In JavaScript, arrays are objects. That means that arrays also have some built-in properties and methods
+
+//Arrays signal that the assigned values all belong to a group. These values are ordered in a sequence and each one can be accessed through its index number in that sequence. In this video, you learned how to build an array in JavaScript. You also learned how JavaScript sequences array items using an index value.
+
+// Understanding push() or Pop() Method in Array
+
+// Push() or Pop()
+// var $push = [];
+// $push.push("Apple");
+// $push.push("Mango");
+// $push.push("Graps");
+
+// $push.pop();
+// console.log($push);
+
+function wall(one, two, three, four) {
+  var arr = [];
+  arr.push(one);
+  arr.push(two);
+  arr.push(three);
+  arr.push(four);
+  console.log(arr);
+}
+
+console.log("itachi", "Sasuke", "Madara", "Aizen");
