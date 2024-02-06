8a93de613cd09d1de905dd672dd662a0b8b099c4
krish <youremail@example.com>
2024-02-06 11:27:47 +0530

    Defensive programming

--- DefensiveProgramming.js
+++ DefensiveProgramming.js
@@ -1,2 +1,24 @@
 //Defensive programming is all about assuming that all the arguments a function will receive are of the wrong type,  the wrong value or both.
 //In other words, you are assuming that things will go wrong and you are proactive in thinking about such scenarios before they happen, so as to make your function less likely to cause errors because of faulty inputs.
+
+
+function letterFinder(word, match) {
+  var condition1 = typeof word == "string" && word.length >= 2; //if the word is a string and the length is greater than or equal to 2
+  var condition2 = typeof match == "string" && match.length == 1; //if the match is a string and the length is equal to 1
+  if (condition1 && condition2) {
+    //if both condition matches
+    for (var i = 0; i < word.length; i++) {
+      if (word[i] == match) {
+        //check if the character at this i position in the word is equal to the match
+        console.log("Found the", match, "at", i);
+      } else {
+        console.log("---No match found at", i);
+      }
+    }
+  } else {
+    //if the requirements don't match
+    console.log("Please pass correct arguments to the function");
+  }
+}
+letterFinder([], []);
+letterFinder("cat", "c");
