293bb5b1f61bc6e8dc6483f82f5ecda221ee5c7d
Krish <101823293+KrishDevCrafting@users.noreply.github.com>
2024-02-06 08:35:27 +0530

    Create DefensiveProgramming.js

---
+++ DefensiveProgramming.js
@@ -0,0 +1,2 @@
+//Defensive programming is all about assuming that all the arguments a function will receive are of the wrong type,  the wrong value or both.
+//In other words, you are assuming that things will go wrong and you are proactive in thinking about such scenarios before they happen, so as to make your function less likely to cause errors because of faulty inputs.
