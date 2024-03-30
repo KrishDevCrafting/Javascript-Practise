a0c6d7232f1bdaf0c1170c0faa2ec69debb63123
Krish <Krishkumer150@gamil.com>
2024-03-30 17:30:40 +0530

    hello-world

--- HoliDay.js
+++ HoliDay.js
@@ -9,9 +9,6 @@
 
 // Celebrate();
 
-// what we have 12345
-// what the output if the k = 2 the output is 34512
-
 // const Code = (arry) => {
 //   var arry = [1, 2, 3, 4, 5];
 //   let k = 2;
@@ -33,23 +30,16 @@
 // };
 
 // work();
-console.log(Math.floor(1, 2));
-
-function DeepakGay() {
-  let deepak = prompt();
-  var deepak2 = prompt();
-  var deepak3 = prompt();
-  if (deepak === "gay") {
-    alert("Deepak pahari is Gay............!");
+//console.log(Math.floor(1, 2));
+// what we have 12345
+// what the output if the k = 2 the output is 34512
+function loop(i) {
+  while (i > 0) {
+    console.log(i);
+    i--;
   }
-  if (deepak2 == 69) {
-    alert("deepak gay hai");
-  }
-  if (deepak3 === "Rachit") {
-    alert("dono Gay hai");
-  } else {
-    alert("Every thing is ok");
-  }
+  return i;
 }
 
-DeepakGay();
+console.log(loop(20));
+//reverse loop hai
