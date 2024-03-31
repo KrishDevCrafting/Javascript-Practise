3ae3005fde1c000d055e796b7f0712313042c90f
Krish <101823293+KrishDevCrafting@users.noreply.github.com>
2024-03-31 12:44:31 +0530

    Create Day2.js
    (LeetCode Questions 2)

---
+++ LeetCodeQuestion/Day2.js
@@ -0,0 +1,13 @@
+//Given an integer n, return a counter function. This counter function initially returns n and then returns 1 more than the previous value every subsequent time it is called (n, n + 1, n + 2, etc).
+
+//Easy question hai
+var createCounter = function (n) {
+  return function () {
+    return n++;
+  };
+};
+
+const num = createCounter(10);
+console.log(num);
+console.log(num);
+console.log(num);
