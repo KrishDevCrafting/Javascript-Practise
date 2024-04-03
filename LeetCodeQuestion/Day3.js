a9857144c374825abe2f3834487d3b611c1674e0
Krish <Krishkumer150@gamil.com>
2024-04-03 08:54:36 +0530

    Closures..1

--- LeetCodeQuestion/Day3.js
+++ LeetCodeQuestion/Day3.js
@@ -1,1 +1,48 @@
+//To Be Or Not To Be
 
+// Write a function expect that helps developers test their code. It should take in any value val and return an object with the following two functions.
+
+// toBe(val) accepts another value and returns true if the two values === each other. If they are not equal, it should throw an error "Not Equal".
+// notToBe(val) accepts another value and returns true if the two values !== each other. If they are equal, it should throw an error "Equal".
+/**
+ * @param {string} val
+ * @return {Object}
+ */
+var expect = function (val) {
+  var val = {
+    a: 5,
+  };
+
+  if (val.a === 5) {
+    console.log("work");
+  } else {
+    console.log("not work..!");
+  }
+};
+
+function tobe(obj) {
+  var obj = {
+    a: 5,
+  };
+}
+5;
+/**
+ * expect(5).toBe(5); // true
+ * expect(5).notToBe(5); // throws "Equal"
+ */
+//function Hosting
+// console.log(sum(2, 2));
+// function sum(a, b) {
+//   return a + b;
+// }
+
+//Importand Topic Closures
+function createAdder(a) {
+  function f(b) {
+    const sum = a + b;
+    return sum;
+  }
+  return f;
+}
+const f = createAdder(3);
+console.log(f(4)); // 7
