8362b09f4a8c74d081503487cfe26984d26df86e
Krish <Krishkumer150@gamil.com>
2024-04-03 12:56:01 +0530

    Updated...

---
+++ LeetCodeQuestion/Day1.js
@@ -0,0 +1,21 @@
+// Day one
+// Create Hello World Function
+//Write a function createHelloWorld. It should return a new function that always returns "Hello World".
+
+// var createHelloWorld = function () {
+//   return function () {
+//     return "Hello World..!";
+//   };
+// };
+
+// const f = createHelloWorld();
+// console.log(f());
+
+// let num = function () {
+//   return function () {
+//     return "Deepak Bhai";
+//   };
+// };
+
+// let a = num();
+// console.log(a());
