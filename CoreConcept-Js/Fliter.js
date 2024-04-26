ec3dad0700bc39926a76e6efcde9fa87df0666b6
Krish <Krishkumer150@gamil.com>
2024-04-26 23:53:43 +0530

    Map

---
+++ CoreConcept-Js/Fliter.js
@@ -0,0 +1,11 @@
+let hold = [-1, -12, -14, -16, 20];
+
+let print$ = hold.filter((num) => num > 0);
+
+console.log(print$);
+
+let UseMap = [2, 3, 4, 5, 6];
+
+let Print2 = UseMap.map((fn) => fn - 1);
+
+console.log(Print2);
--- CoreConcept-Js/ProtoTypes.js
+++ CoreConcept-Js/ProtoTypes.js
@@ -1,1 +1,24 @@
+function Person(name) {
+  this.name = name;
+}
 
+Person.prototype.sayHello = function () {
+  console.log(`Hello, my name is ${this.name}.`);
+};
+
+const person1 = new Person("Alice");
+person1.sayHello(); // Output: Hello, my name is Alice.
+
+function Age(look) {
+  this.look = look;
+}
+
+Age.prototype.where = function () {
+  console.log(`You're age is ${this.look}`);
+};
+const hold = new Age(12);
+hold.where();
+
+// const person2 = Object.create(Person.prototype);
+// person2.name = "Bob";
+// person2.sayHello(); // Output: Hello, my name is Bob.
