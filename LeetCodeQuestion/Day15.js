1af7282f2c00e186d1984375f797e307cc6bb1b9
Krish <Krishkumer150@gamil.com>
2024-04-29 13:09:59 +0530

    EasyQ

--- LeetCodeQuestion/Day15.js
+++ LeetCodeQuestion/Day15.js
@@ -1,7 +1,16 @@
-function Star(){
+// 2621. Sleep
+//Given a positive integer millis, write an asynchronous function that sleeps for millis milliseconds. It can resolve any value.
 
-  console.log("I'm StarBoy..!")
+/**
+ * @param {number} millis
+ * @return {Promise}
+ */
+async function sleep(millis) {
+
+  let value  = new Promise((resolve,reject)=>{
+    
+  })
 }
 
-
-Star()
+let t = Date.now();
+sleep(100).then(() => console.log(Date.now() - t)); // 100
