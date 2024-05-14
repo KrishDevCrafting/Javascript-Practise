389865b3af96b84d40d3ed6436b37afe09a6f014
Krish <Krishkumer150@gamil.com>
2024-05-14 09:42:51 +0530

    Loop

--- LeetCodeQuestion/InterviewQ/ArrayManipulation.js
+++ LeetCodeQuestion/InterviewQ/ArrayManipulation.js
@@ -69,8 +69,16 @@ console.log(reversedArray);
 
 const Method2 = [1, 2, 3, 4, 5, 6, 7, 8, 9, 10];
 const empty = [];
-for (let a = Method2.length - 2; a >= 0; a--) {
+for (let a = Method2.length - 1; a >= 0; a--) {
   empty.push(Method2[a]);
 }
 
 console.log(empty);
+
+const master = [10, 9, 8, 7, 6, 5, 4, 3, 2, 1];
+const etySpace = [];
+for (let fn = master.length - 1; fn > 0; fn--) {
+  etySpace.push(master[fn]);
+}
+
+console.log(etySpace);
