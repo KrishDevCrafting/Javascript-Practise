2d5af90df253488ae4c06a436723611cfbbd2a07
Krish <Krishkumer150@gamil.com>
2024-05-19 12:48:58 +0530

    Classes

--- CoreConcept-Js/Classes.js
+++ CoreConcept-Js/Classes.js
@@ -8,7 +8,9 @@ class Person {
 
   // Method to greet
   greet() {
-    console.log(`Hello, my name is ${this.name} and I am ${this.age} years old.`);
+    console.log(
+      `Hello, my name is ${this.name} and I am ${this.age} years old.`
+    );
   }
 
   // Method to increment age
@@ -19,9 +21,25 @@ class Person {
 }
 
 // Create an instance of the class
-const person1 = new Person('Alice', 30);
+const person1 = new Person("Alice", 30);
 
 // Use the class methods
 person1.greet(); // Output: Hello, my name is Alice and I am 30 years old.
 person1.haveBirthday(); // Output: Happy birthday! I am now 31 years old.
 
+//___________________________________________________________________________________________________________//
+
+// Practise What i know
+class Car {
+  constructor(a, b) {
+    this.a = a;
+    this.b = b;
+    this.add = a + b;
+  }
+  call() {
+    console.log(`added Number ${this.add}`);
+  }
+}
+
+const outPut = new Car(2, 2);
+outPut.call();
