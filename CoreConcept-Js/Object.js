d083ba2413d32495f3426fa1c092402775492fd5
KrishDevCrafting <krishkumer150@gmail.com>
2024-07-16 00:00:17 +0530

    where

--- CoreConcept-Js/Object.js
+++ CoreConcept-Js/Object.js
@@ -27,3 +27,44 @@ console.log(access);
 for (var key in lassi) {
   console.log(key);
 }
+
+// // “this” in methods
+// It’s common that an object method needs to access the information stored in the object to do its job.
+
+// For instance, the code inside user.sayHi() may need the name of the user.
+
+// To access the object, a method can use the this keyword.
+
+// The value of this is the object “before dot”, the one used to call the method.
+
+// For instance:
+
+// let user = {
+//   name: "jeo",
+//   age: "18",
+//   sayhi() {
+//     console.log(this.age);
+//   },
+// };
+
+// user.sayhi();
+ //use fo this
+// this is a special keyword that refers to the context in which the current code is executing. 
+ const obj = {
+  value: 42,
+  method: function() {
+    console.log(this.value); // obj.value
+
+    function innerFunction() {
+      console.log(this.value); // undefined in strict mode, global object in non-strict mode
+    }
+    innerFunction();
+
+    const arrowFunction = () => {
+      console.log(this.value); // obj.value (lexically inherited)
+    };
+    arrowFunction();
+  }
+};
+
+obj.method();
