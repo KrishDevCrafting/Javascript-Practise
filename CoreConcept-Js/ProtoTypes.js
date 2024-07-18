53cfd6b8b0aed5c009e8d61e82bb55a3a0d93252
KrishDevCrafting <krishkumer150@gmail.com>
2024-07-18 22:30:08 +0530

    Love You

--- CoreConcept-Js/ProtoTypes.js
+++ CoreConcept-Js/ProtoTypes.js
@@ -6,7 +6,7 @@ Person.prototype.sayHello = function () {
   console.log(`Hello, my name is ${this.name}.`);
 };
 
-const person1 = new Person("Alice");
+const person1 = new Person("Krishu");
 person1.sayHello(); // Output: Hello, my name is Alice.
 
 function Age(look) {
@@ -22,3 +22,13 @@ hold.where();
 // const person2 = Object.create(Person.prototype);
 // person2.name = "Bob";
 // person2.sayHello(); // Output: Hello, my name is Bob.
+
+// In JavaScript, objects have a special hidden property [[Prototype]] (as named in the specification), that is either null or references another object. That object is called “a prototype”:
+let animal = {
+  eats: true
+};
+let rabbit = {
+  jumps: true
+};
+
+rabbit.__proto__ = animal; // 
