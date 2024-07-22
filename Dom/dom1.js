91103ea5da0f9ef5c8bab1462482de28cdc32bde
Krish <Krishkumer150@gamil.com>
2024-07-22 22:13:17 +0530

    Work

--- Dom/dom1.js
+++ Dom/dom1.js
@@ -2,7 +2,12 @@ function $var() {
   return (document.getElementById("box").textContent = "hello,world");
 }
 
+// let text = document.body.firstChild;
+// alert(text.data); //Riya
+
+// let comment = text.nextSibling;
+// alert(comment.data); // Comment
 // alert(location.href);
 // document.body.DOCUMENT_NODE;
 
-alert(elem.outerHTML);
+// alert(elem.outerHTML);
