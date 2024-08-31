65b093f3ef365f9a89f779e7f9fb476587e9f78a
Krish <krishkumer150@gmail.com>
2024-08-31 16:34:45 +0530

    Js

---
+++ DiceBootcampJs/Day4.js
@@ -0,0 +1,25 @@
+const n = 7;
+function milton(n) {
+  if (n <= 1) {
+    return console.log("not a Prime-number!");
+  }
+
+  for (let i = 0; i < n; i++) {
+
+    
+  }
+}
+
+// function isPrime(n) {
+//   // Corner case
+//   if (n <= 1) return false;
+
+//   // Check from 2 to n-1
+//   for (let i = 2; i < n; i++) if (n % i == 0) return false;
+
+//   return true;
+// }
+
+// // Driver Code
+
+// isPrime(11) ? console.log("true") : console.log("false");
