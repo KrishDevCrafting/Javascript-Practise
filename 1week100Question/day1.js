1828716cb2b76941edbb875b8ad4c018324ee637
Krish <krishkumer150@gmail.com>
2024-09-04 15:42:56 +0530

    Frontend

--- 1week100Question/day1.js
+++ 1week100Question/day1.js
@@ -1,27 +1,52 @@
 // started problem solving the question for logic buliding!
 // Question1 Write a JavaScript function to check if a number is prime.
 
-const primeValue = 2;
-function CheckPrimeN(primeValue) {
-  if (primeValue) {
+// const primeValue = 2;
+// function CheckPrimeN(primeValue) {
+//   if (primeValue) {
+//   }
+// }
+
+// function isPrime(num) {
+//   // Handle special cases
+//   if (num <= 1) return false; // Numbers less than or equal to 1 are not prime
+//   if (num <= 3) return true; // 2 and 3 are prime numbers
+
+//   // Eliminate multiples of 2 and 3
+//   if (num % 2 === 0 || num % 3 === 0) return false;
+
+//   // Check for factors from 5 to the square root of num
+//   for (let i = 5; i * i <= num; i += 6) {
+//     if (num % i === 0 || num % (i + 2) === 0) return false;
+//   }
+
+//   return true; // num is a prime number
+// }
+// const hold = isPrime;
+
+// console.log(hold());
+
+// Implement a JavaScript function to reverse a string.
+
+const reverseString = (value) => {
+  var empty = "";
+  for (let i = value.length - 1; i >= 0; i--) {
+    empty += value[i];
+  }
+  return empty;
+};
+
+console.log(reverseString("Krish"));
+
+function checknumber(num) {
+  if (num % 2 == 0) {
+    return true;
+  } else {
+    return false;
   }
 }
 
-function isPrime(num) {
-  // Handle special cases
-  if (num <= 1) return false; // Numbers less than or equal to 1 are not prime
-  if (num <= 3) return true; // 2 and 3 are prime numbers
+console.log(checknumber(44));
 
-  // Eliminate multiples of 2 and 3
-  if (num % 2 === 0 || num % 3 === 0) return false;
 
-  // Check for factors from 5 to the square root of num
-  for (let i = 5; i * i <= num; i += 6) {
-    if (num % i === 0 || num % (i + 2) === 0) return false;
-  }
-
-  return true; // num is a prime number
-}
-const hold = isPrime;
-
-console.log(hold());
+// Write a JavaScript function to print the numbers from 1 to 100. But for multiples of 3, print "Fizz" instead of the number, and for multiples of 5, print "Buzz". For numbers which are multiples of both 3 and 5, print "FizzBuzz".
