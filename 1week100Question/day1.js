6723a897cf8a3688211731220de9a128239a0910
Krish <krishkumer150@gmail.com>
2024-09-04 15:47:30 +0530

    Frontend

--- 1week100Question/day1.js
+++ 1week100Question/day1.js
@@ -48,5 +48,30 @@ function checknumber(num) {
 
 console.log(checknumber(44));
 
-
 // Write a JavaScript function to print the numbers from 1 to 100. But for multiples of 3, print "Fizz" instead of the number, and for multiples of 5, print "Buzz". For numbers which are multiples of both 3 and 5, print "FizzBuzz".
+
+// function logicalQ() {
+//   for (let i = 0; i <= 100; i++) {
+//     if (i % 3 === 0 && i % 5 === 0) {
+//       console.log("FizzBuzz");
+//     }else{
+
+//     }
+//   }
+// }
+
+function fizzBuzz() {
+  for (let i = 1; i <= 100; i++) {
+    if (i % 3 === 0 && i % 5 === 0) {
+      console.log("FizzBuzz");
+    } else if (i % 3 === 0) {
+      console.log("Fizz");
+    } else if (i % 5 === 0) {
+      console.log("Buzz");
+    } else {
+      console.log(i);
+    }
+  }
+}
+
+fizzBuzz();
