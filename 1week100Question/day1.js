c6552db24fbf16053fb652e386463ce8572f21e2
Krish <krishkumer150@gmail.com>
2024-09-06 22:42:06 +0530

    js-logic

--- 1week100Question/day1.js
+++ 1week100Question/day1.js
@@ -1,30 +1,25 @@
 // started problem solving the question for logic buliding!
 // Question1 Write a JavaScript function to check if a number is prime.
 
-// const primeValue = 2;
-// function CheckPrimeN(primeValue) {
-//   if (primeValue) {
-//   }
-// }
 
-// function isPrime(num) {
-//   // Handle special cases
-//   if (num <= 1) return false; // Numbers less than or equal to 1 are not prime
-//   if (num <= 3) return true; // 2 and 3 are prime numbers
+function isPrime(num) {
+  // Handle special cases
+  if (num <= 1) return false; // Numbers less than or equal to 1 are not prime
+  if (num <= 3) return true; // 2 and 3 are prime numbers
 
-//   // Eliminate multiples of 2 and 3
-//   if (num % 2 === 0 || num % 3 === 0) return false;
+  // Eliminate multiples of 2 and 3
+  if (num % 2 === 0 || num % 3 === 0) return false;
 
-//   // Check for factors from 5 to the square root of num
-//   for (let i = 5; i * i <= num; i += 6) {
-//     if (num % i === 0 || num % (i + 2) === 0) return false;
-//   }
+  // Check for factors from 5 to the square root of num
+  for (let i = 5; i * i <= num; i += 6) {
+    if (num % i === 0 || num % (i + 2) === 0) return false;
+  }
 
-//   return true; // num is a prime number
-// }
-// const hold = isPrime;
+  return true; // num is a prime number
+}
+const hold = isPrime;
 
-// console.log(hold());
+console.log(hold());
 
 // Implement a JavaScript function to reverse a string.
 
@@ -66,3 +61,11 @@ const logicQ = (n) => {
 
 const holdValue = logicQ;
 console.log(holdValue(20));
+
+// Write a function to find the greatest common divisor (GCD) of two numbers in JavaScript.
+
+const GCD = (dv) => {
+  for (let b = 1; b <= dv; b++) {
+    if(b%dv)
+  }
+};
