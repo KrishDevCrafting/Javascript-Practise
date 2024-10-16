9d7ed263b2bd2ba0134c660840d8e580bbfeb36b
Krish <krishkumer150@gmail.com>
2024-10-16 19:38:29 +0530

    js

--- 1week100Question/day2.js
+++ 1week100Question/day2.js
@@ -1,1 +1,15 @@
+function findThePrime(num) {
+  if (num < 1) return false;
+  if (num < 3) return true;
 
+  if (num % 2 == 0 || num % 3 == 0) {
+    return false;
+  }
+  // find the multipe of 5
+  for (let i = 5; i * i <= num; i += 6) {
+    if (num % i === 0 || num % (i + 2) === 0) return false;
+  }
+  return true;
+}
+
+console.log(findThePrime(2));
