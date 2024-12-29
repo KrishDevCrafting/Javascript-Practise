0e11c1afaaf477302250d92648422df2f9ec2806
KrishDevCrafting <krishkumer150@gmail.com>
2024-12-29 21:17:51 +0530

    fs

--- LastYearPractise/logicQuestions.js
+++ LastYearPractise/logicQuestions.js
@@ -66,5 +66,31 @@ function largestNumber(nb1, nb2, nb3) {
 
   return largest;
 }
-console.log(largestNumber(10,20,30));
+console.log(largestNumber(10, 20, 30));
 
+let deadline = new Date("dec 31, 2024 13:37:25").getTime();
+
+// To call defined fuction every second
+let x = setInterval(function () {
+  // Getting current time in required format
+  let now = new Date().getTime();
+
+  // Calculating the difference
+  let t = deadline - now;
+
+  // Getting value of days, hours, minutes, seconds
+  let days = Math.floor(t / (1000 * 60 * 60 * 24));
+  let hours = Math.floor((t % (1000 * 60 * 60 * 24)) / (1000 * 60 * 60));
+  let minutes = Math.floor((t % (1000 * 60 * 60)) / (1000 * 60));
+  let seconds = Math.floor((t % (1000 * 60)) / 1000);
+
+  // Output the remaining time
+  document.getElementById("demo").innerHTML =
+    days + "d " + hours + "h " + minutes + "m " + seconds + "s ";
+
+  // Output for over time
+  if (t < 0) {
+    clearInterval(x);
+    document.getElementById("demo").innerHTML = "EXPIRED";
+  }
+}, 1000);
