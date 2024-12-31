cc92eab9fd9a53e3c2ba0ef4aae863a334280428
Krish <krishkumer150@gmail.com>
2025-01-01 00:26:38 +0530

    up

--- 1week100Question/Practise2.js
+++ 1week100Question/Practise2.js
@@ -22,3 +22,17 @@ console.log(loopReverse("BMW M5 Serires!"));
 // commit+++++++++++++++++
 
 // last commit
+
+function loopReverse(b) {
+  let emptyVariable = "";
+
+  for (let a = b.length - 1; a >= 0; a--) {
+    emptyVariable += b[a];
+    console.log(
+      `Index: ${a}, Character: ${b[a]}, Current reversed string: ${emptyVariable}`
+    );
+  }
+  return emptyVariable;
+}
+
+console.log(loopReverse("BMW M5 Serires!"));
