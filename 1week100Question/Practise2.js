a37d78c4af74e199c68c06088409a225065c0fc6
Krish <krishkumer150@gmail.com>
2025-01-09 21:52:02 +0530

    up

--- 1week100Question/Practise2.js
+++ 1week100Question/Practise2.js
@@ -56,3 +56,7 @@ console.log(findLargestNumber([1, 2, 3, 4, 5])); // Output: 5
 console.log(findLargestNumber([-10, -20, -30, -5])); // Output: -5
 console.log(findLargestNumber([100, 200, 300, 400, 500])); // Output: 500
 console.log(findLargestNumber([])); // Output: null
+
+
+
+//hello world!
