545c93223c93a1c19092411931f1314478c867dd
KrishDevCrafting <krishkumer150@gmail.com>
2025-06-15 21:58:07 +0530

    Question-1

--- javascript/lesson1.js
+++ javascript/lesson1.js
@@ -5,4 +5,23 @@ function filterList(arr) {
 
 console.log(filterList([1, 2, "a", "b"])); // ➞ [1, 2]
 console.log(filterList([1, "a", "b", 0, 15])); // ➞ [1, 0, 15]
-console.log(filterList(["x", 23, "y", 42])); 
+console.log(filterList(["x", 23, "y", 42]));
+
+// Given two integers a and b, which can be positive or negative, find the sum of all the integers between and including them and return it. If the two numbers are equal return a or b.
+// Note: a and b are not ordered!
+
+function getsum(a, b) {
+  let min = Math.min(a, b);
+  let max = Math.max(a, b);
+  let sum = 0;
+  for (let i = min; i <= max; i++) {
+    sum += i;
+  }
+  return sum;
+}
+
+console.log(getsum(1, 0)); // ➞ 1 (1 + 0)
+console.log(getsum(1, 2)); // ➞ 3 (1 + 2)
+console.log(getsum(0, 1)); // ➞ 1 (0 + 1)
+console.log(getsum(1, 1)); // ➞ 1
+console.log(getsum(-1, 2)); // ➞ 2 (-1 + 0 + 1 + 2)
